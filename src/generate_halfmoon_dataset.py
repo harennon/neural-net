f4fff50ef4ebbc8ec19e0e4296c87acbad6925e7
Neil Jurling <neilj.harennon@gmail.com>
2018-12-17 07:45:43 -0800

    Fixed testing issue with half moon. Going to start on batch learning

--- src/generate_halfmoon_dataset.py
+++ src/generate_halfmoon_dataset.py
@@ -16,25 +16,25 @@ X_test, y_test = generate_halfmoon_dataset(noise = 0.1)
 nn = NeuralNetwork([2, 4, 2, 1], 0.03)
 if (not os.path.isfile("nn_halfmoon_noise_0.1.npy")):
     train = [X_train, y_train]
-    nn.train_network(train, n_epochs = 0, threshold = 0.01)
+    nn.train_network(train, n_epochs = 0, threshold = 0.001)
     np.save("nn_halfmoon_noise_0.1", nn.get_network())
 else :
     W = np.load("nn_halfmoon_noise_0.1.npy")
-    print(W)
+    print("loaded weight matrix W = %s\n" %(W))
     nn.load_network(W)
 
-y_train_test = []
-for i in range(len(y_train)):
-    y_train_test.append(np.squeeze(nn.predict(X_train[i])))
-
 y_test_test = []
+for i in range(len(y_test)):
+    y_test_test.append(np.around(np.squeeze(nn.predict(X_test[i]))))
+
+y_train_test = []
 for j in range(len(y_test)):
-    y_test_test.append((np.squeeze(nn.predict(X_test[i])))) #its not the nn that's training incorrectly, something's not getting reinitialized after predict
+    y_train_test.append(np.around(np.squeeze(nn.predict(X_train[j]))))
 
 
 plt.subplot(221)
-plt.title("Training Data")
-plt.scatter(X_train[:,0], X_train[:,1], c=y_train, s=40)
+plt.title("Train Data")
+plt.scatter(X_test[:,0], X_test[:,1], c=y_train, s=40)
 
 plt.subplot(222)
 plt.title("Testing Training Data")
@@ -45,8 +45,9 @@ plt.title("Test Data")
 plt.scatter(X_test[:,0], X_test[:,1], c=y_test, s=40)
 
 plt.subplot(224)
-plt.title("Testing Test Data")
+plt.title("Testing test Data")
 plt.scatter(X_test[:,0], X_test[:,1], c=y_test_test, s=40)
+
 
 plt.subplots_adjust(hspace = 0.4, wspace = 0.4)
 plt.show()
